2ae60f6bedda129e433f6581d42a6c41511411c8
Matthias <matthias.jofre@gmail.com>
2022-10-18 13:13:04 -0300

    update src/router/index.ts and HomePage.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
+import BarraBusqueda from '@/views/BarraBusquedaPage.vue'
+import Pagina from '@/views/PaginaPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +13,16 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/busqueda',
+    name: 'BarraBusqueda',
+    component: BarraBusqueda
+  },
+  {
+    path: '/pagina',
+    name: 'Pagina',
+    component: Pagina
   }
 ]
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,45 +2,60 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <div class="inicio">
+          <a href="https://ionicframework.com/docs/components">
+            <img
+              class="Logo"
+              alt="Logo"
+              src="@/../public/assets/icon/Logo_nuevo_ucen1.png"
+            />
+          </a>
+          <ion-title><h1>Mi Listado</h1></ion-title>
+        </div>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
+          <Menu></Menu>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <img class="une" src="@/../public/assets/icon/VK1.png" />
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+  },
 });
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,13 +71,28 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
 #container a {
   text-decoration: none;
+}
+
+.inicio {
+  background: blue;
+  display: block;
+  box-shadow: 4px 4px 4px 2px rgba(0, 0, 0, 0.4);
+}
+.Logo {
+  margin: 0.5%;
+  border-radius: 100%;
+  width: 6%;
+}
+h1 {
+  display: inline-block;
+  background: green;
 }
 </style>
